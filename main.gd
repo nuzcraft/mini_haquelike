ef9303a273ca9494b076abd59c5d3922a35821a8
nuzcraft <nuzcraft@gmail.com>
2022-11-19 21:53:29 -0600

    added gdignore for research folder

--- main.gd
+++ main.gd
@@ -5,11 +5,10 @@ var tiles = [];
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
+	# create a simple 2d array to hold our initial map of tiles 
 	for i in 10:
 		tiles.append([])
 		for j in 10:
 			tiles[i].append(Tile.new())
 			
-	print(tiles)
-	print(tiles[0][9].isExplored)
-	
+
