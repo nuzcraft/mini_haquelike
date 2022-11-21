6ffb20e425a2e036e453c3b6cbe133792256a795
nuzcraft <nuzcraft@gmail.com>
2022-11-21 07:13:17 -0600

    added initial tile script

--- main.gd
+++ main.gd
@@ -9,6 +9,5 @@ func _ready():
 	for i in 10:
 		tiles.append([])
 		for j in 10:
-			tiles[i].append(Tile.new())
-			
+			tiles[i].append(Tile.new(i, j))
 
--- scripts/tile.gd
+++ scripts/tile.gd
@@ -1,10 +1,36 @@
 class_name Tile
 
-var isExplored: bool
-var isWalkable: bool
-var isVisible: bool
+var x: int setget ,get_x
+var y: int setget, get_y
+var is_explored: bool = false setget set_is_explored,get_is_explored
+var is_walkable: bool = true setget set_is_walkable,get_is_walkable
+var is_visible: bool = false setget set_is_visible,get_is_visible
 
-func _init(explored: bool = true, walkable: bool = true, visible: bool = true):
-	isExplored = explored;
-	isWalkable = walkable;
-	isVisible = visible;
+func _init(x_param, y_param):
+	x = x_param
+	y = y_param
+
+# setters and getters
+func get_x():
+	return x
+	
+func get_y():
+	return y
+
+func set_is_explored(value: bool):
+	is_explored = value
+	
+func get_is_explored() -> bool:
+	return is_explored
+	
+func set_is_walkable(value: bool):
+	is_walkable = value
+	
+func get_is_walkable() -> bool:
+	return is_walkable
+	
+func set_is_visible(value: bool):
+	is_visible = value
+	
+func get_is_visible() -> bool:
+	return is_visible
